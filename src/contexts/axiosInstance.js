dbb07800378ea4f5873c70cbde0fec7dd4b926dd
laxmikantagreeta <laxmikant@agreeta.com>
2025-07-22 13:27:26 +0530

    Update axiosInstance.js

--- src/contexts/axiosInstance.js
+++ src/contexts/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:"http://192.168.1.95:8000", // backend URL
+  baseURL:"http://35.154.64.13:8080",
   // baseURL: "http://localhost:8000", // backend URL
 });
 
