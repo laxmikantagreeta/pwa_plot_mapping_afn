5d3c3b31812ca039633cddec4e2acd6f4b73e051
laxmikantagreeta <laxmikant@agreeta.com>
2025-07-23 12:44:07 +0530

    Update axiosInstance.js

--- src/contexts/axiosInstance.js
+++ src/contexts/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:"https://5px2ad4c13.execute-api.ap-south-1.amazonaws.com",
+  baseURL: "https://5px2ad4c13.execute-api.ap-south-1.amazonaws.com/", // ← add trailing slash
 });
 
 instance.interceptors.request.use((config) => {
