541886c5864c444d1ae1c4599c2483575d608e83
laxmikantagreeta <laxmikant@agreeta.com>
2025-07-23 17:34:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import appLogo from '/favicon.svg'
-// import PWABadge from './PWABadge.jsx'
-// import './App.css'
-// import { Routes, Route } from 'react-router-dom'
-// import LoginWithOTP from './pages/LoginWithOTP.jsx'
-// import PlotMapping from './pages/PlotMapping.jsx'
-// import DashboardPage from './pages/DashboardPage.jsx'
-// import ForecastTable from './pages/ForecastTable.jsx'
-// import FarmerProfile from './pages/FarmerProfile.jsx'
-// import SettingsPage from './pages/SettingsPage.jsx'
-// import ProtectedRoute from "./components/ProtectedRoute";
-
-// function App() {
-
-//   return (
-//     // <FarmerProvider>
-//       <Routes>
-//         <Route path="/" element={<LoginWithOTP />} />
-//         {/* <Route path="/" element={<Login />} /> */}
-//         <Route path="/DashboardPage" element={<DashboardPage />} />
-//         <Route path="/FarmerProfile" element={<FarmerProfile />} />
-//         <Route path="/ForecastTable" element={<ForecastTable />} />
-//         <Route path="/SettingsPage" element={<SettingsPage />} />
-//         <Route path="/plot" element={<PlotMapping />} />
-//         {/* <Route path="*" element={<Navigate to="/" />} /> */}
-//       </Routes>
-//     // </FarmerProvider>
-//   )
-// }
-
-// export default App
-
-// {/* <Route
-//   path="/DashboardPage"
-//   element={accessToken ? <DashboardPage /> : <Navigate to="/" />}
-// /> */}
-
 
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
@@ -47,11 +8,12 @@ import ForecastTable from './pages/ForecastTable.jsx';
 import FarmerProfile from './pages/FarmerProfile.jsx';
 import SettingsPage from './pages/SettingsPage.jsx';
 import ProtectedRoute from "./components/ProtectedRoute";
-
+import useStatusBarColor from './components/useStatusBarColor.js';
 // import { AuthProvider } from './contexts/AuthContext';
 // import { FarmerProvider } from './contexts/FarmerContext';
 
 function App() {
+  useStatusBarColor()
   return (
     // <AuthProvider>
     //   <FarmerProvider>
