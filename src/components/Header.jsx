5c5aaf09b94909f9b3a1442706b6ce2dfddf3343
laxmikantagreeta <laxmikant@agreeta.com>
2025-07-23 14:36:41 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,3 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-// import { FaUser } from "react-icons/fa";
-
-// const Header = ({ title = "शेतकरी डॅशबोर्ड" }) => {
-//   const navigate = useNavigate();
-
-//   return (
-//     <header className="bg-green-500 text-white px-4 py-3 flex justify-between items-center">
-//       {/* Menu Icon */}
-//       <button
-//         onClick={() => navigate("/SettingsPage")}
-//         className="hover:cursor-pointer"
-//         aria-label="Open Settings"
-//       >
-//         <span className="text-[32px] leading-none">☰</span>
-//       </button>
-
-//       {/* Title */}
-//       <h1 className="text-lg font-semibold">{title}</h1>
-
-//       {/* User Profile */}
-//       <button
-//         onClick={() => navigate("/FarmerProfile")}
-//         className="text-2xl text-white hover:cursor-pointer"
-//         aria-label="Farmer Profile"
-//       >
-//         <FaUser />
-//       </button>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
 
 import React from "react";
 import { useNavigate } from "react-router-dom";
