47603c04663a9f8dcb1b2db6b621ed66a25b401e
laxmikantagreeta <laxmikant@agreeta.com>
2025-07-25 15:53:17 +0530

    Update useStatusBarColor.js

--- src/components/useStatusBarColor.js
+++ src/components/useStatusBarColor.js
@@ -1,18 +1,33 @@
+
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const useStatusBarColor = () => {
   const location = useLocation();
 
-  useEffect(() => {
+  const updateThemeColor = () => {
     const metaThemeColor = document.querySelector('meta[name="theme-color"]');
     if (!metaThemeColor) return;
 
     if (location.pathname === "/") {
-      metaThemeColor.setAttribute("content", "#f0fdf4"); // white
+      metaThemeColor.setAttribute("content", "#f0fdf4");
     } else {
-      metaThemeColor.setAttribute("content", "#22c55e"); 
+      metaThemeColor.setAttribute("content", "#22c55e");
     }
+  };
+
+  useEffect(() => {
+    updateThemeColor(); // initial render
+
+    document.addEventListener("visibilitychange", () => {
+      if (document.visibilityState === "visible") {
+        updateThemeColor();
+      }
+    });
+
+    return () => {
+      document.removeEventListener("visibilitychange", updateThemeColor);
+    };
   }, [location]);
 };
 
